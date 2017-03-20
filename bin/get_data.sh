8e00f779d928711387a8d2210077de88fa29df4b
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-03-20 14:12:00 +0100

    Change downloaded dataset

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 mkdir data
-mkdir data/gminas
-mkdir data/circuits
 
-
-# Save into the data/gminas directory all .xls files linked on this page
-wget -q -P data/gminas -r -nd -np -l 1 -A ".xls" \
-    http://prezydent2000.pkw.gov.pl/gminy/gminy.html
-
-# Save into the data/circuits directory all .xls files linked on this page
-wget -q -P data/circuits -r -nd -np -l 1 -A ".xls" \
+# Save into the data directory all .xls files linked on this page
+wget -q -P data -r -nd -np -l 1 -A ".xls" \
     http://prezydent2000.pkw.gov.pl/gminy/obwody.html
+
+wget -q -P data prezydent2000.pkw.gov.pl/gminy/zal1.xls
